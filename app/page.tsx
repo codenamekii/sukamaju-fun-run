572719a4fc33d41cc4135d1a5c51a9b2d64d2fc3
codenamekii <magnoncro95@gmail.com>
2025-08-13 11:11:03 +0800

    minor update

--- app/page.tsx
+++ app/page.tsx
@@ -448,7 +448,7 @@ export default function SponsorshipSection() {
                     <h3 className="text-xl font-bold text-white mb-4">{item.category}</h3>
 
                     <div className="mb-4">
-                      <p className="text-gray-400 text-sm">Harga Normal</p>
+                      <p className="text-gray-400 text-sm">Night Owl (Sesi kedua)</p>
                       <p className="text-2xl font-bold text-green-400">{item.NormalPrize}</p>
                     </div>
 
